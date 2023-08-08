29ba3762ff3e19e15385ddc5f2b79674ce9d5be8
KenanyaKAP <kenanyaline@gmail.com>
2023-08-08 13:07:25 +0700

    Link to dashboard

--- src/components/buttons/LoginButton.tsx
+++ src/components/buttons/LoginButton.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 const LoginButton = () => {
   return (
-    <Link href='/auth'>
+    <Link href='https://dashboard.mage-ce.id'>
       <button className=' mx-auto w-[90vw] rounded-xl bg-white px-7 py-2 text-violet-900 hover:bg-violet-300 md:w-max  md:rounded-full'>
-        Sign In
+        Dashboard
       </button>
     </Link>
   )
